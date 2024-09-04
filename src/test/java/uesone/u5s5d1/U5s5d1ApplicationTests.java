a04703a3fd631edb5205d741a33ce204aea3b438
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-09-04 17:02:36 +0200

    Update U5s5d1ApplicationTests.java

--- src/test/java/uesone/u5s5d1/U5s5d1ApplicationTests.java
+++ src/test/java/uesone/u5s5d1/U5s5d1ApplicationTests.java
@@ -1,13 +1,20 @@
 package uesone.u5s5d1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import uesone.u5s5d1.entities.Pizza;
+
+import java.util.List;
+
+import static java.awt.SystemColor.menu;
 
 @SpringBootTest
 class U5s5d1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testNumeroPizzeNelMenu() {
+		List<Pizza> pizze = menu.getPizze();
+		Assertions.assertEquals(3, pizze.size(), "Il menu dovrebbe contenere 3 pizze.");
 	}
-
 }
